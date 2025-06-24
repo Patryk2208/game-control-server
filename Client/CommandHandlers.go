81cbade940b0c6cc2a408bc920b88cfa71b2be3f
Patryk Pol <patryk22082005@gmail.com>
2025-06-24 17:11:59 +0200

    Client all basic command dry

--- Client/CommandHandlers.go
+++ Client/CommandHandlers.go
@@ -33,19 +33,69 @@ func ParseLoginReply(reply Reply) bool {
 }
 
 func RegisterCommandHandler(session *Session, command Command) {
-	fmt.Println("register command")
+	message := command.Name
+	for i := 0; i < len(command.Args); i++ {
+		message += " " + command.Args[i]
+	}
+	err := session.ServerConn.WriteMessage(1, []byte(message))
+	if err != nil {
+		return
+	}
+	reply := <-session.ReplyChannel
+	if !ParseRegisterReply(reply) {
+		return
+	}
+	session.Context = NewAuthenticatedContext()
+}
+
+func ParseRegisterReply(reply Reply) bool {
+	if reply.Message[0] == 'T' {
+		fmt.Println("Authentication successful")
+		return true
+	} else {
+		fmt.Println("Authentication failed")
+		return false
+	}
 }
 
 func NormalHelpCommandHandler(session *Session, command Command) {
-	fmt.Println("help command")
+	fmt.Println("You are not logged in")
+	fmt.Println("Usage:")
+	fmt.Println("help")
+	fmt.Println("register <username> <password>")
+	fmt.Println("login <username> <password>")
+	fmt.Println("exit")
 }
 
 func LogoutCommandHandler(session *Session, command Command) {
-	fmt.Println("logout command")
+	err := session.ServerConn.WriteMessage(1, []byte(command.Name))
+	if err != nil {
+		return
+	}
+	reply := <-session.ReplyChannel
+	if !ParseLogoutReply(reply) {
+		return
+	}
+	session.Context = NewNormalContext()
+}
+
+func ParseLogoutReply(reply Reply) bool {
+	if reply.Message[0] == 'T' {
+		fmt.Println("Logout successful")
+		return true
+	} else {
+		fmt.Println("Logout failed")
+		return false
+	}
 }
 
 func AuthenticatedHelpCommandHandler(session *Session, command Command) {
-	fmt.Println("help command")
+	fmt.Println("You are logged in")
+	fmt.Println("Usage:")
+	fmt.Println("help")
+	fmt.Println("logout")
+	fmt.Println("start :: start new singleplayer game")
+	fmt.Println("exit")
 }
 
 func ExitCommandHandler(session *Session, command Command) {
