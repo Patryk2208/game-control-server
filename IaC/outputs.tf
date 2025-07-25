36628b40904aabe21c34d1296ba27fd62e5506b7
Patryk Pol <patryk22082005@gmail.com>
2025-07-25 15:11:28 +0200

    patch CI/CD

--- IaC/outputs.tf
+++ IaC/outputs.tf
@@ -7,3 +7,28 @@ output "enabled_apis" {
   value = [for s in google_project_service.services : s.service]
   description = "List of enabled APIs"
 }*/
+
+output "cluster_name" {
+  value = var.cluster_name
+  description = "Cluster name"
+}
+
+output "zone" {
+  value = var.zone
+  description = "Zone"
+}
+
+output "gcp_github_service_account_email" {
+  value       = google_service_account.github_deployer.email
+  description = "Email of the CI Service Account"
+}
+
+output "gcp_wid_ci_provider" {
+  value = google_iam_workload_identity_pool_provider.github_ci_provider.name
+  description = "Full Workload Identity Provider path for CI"
+}
+
+output "gcp_wid_cd_provider" {
+  value = google_iam_workload_identity_pool_provider.github_cd_provider.name
+  description = "Full Workload Identity Provider path for CD"
+}
